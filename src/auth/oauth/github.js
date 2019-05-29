9ecebb1fb245ace93b3dbdbe6f10de8559c1c529
Chris Kozlowski <kozlowskicd@gmail.com>
2019-05-29 15:54:48 -0700

    refactor

--- src/auth/oauth/github.js
+++ src/auth/oauth/github.js
@@ -4,7 +4,7 @@ const superagent = require('superagent');
 const Users = require('../users-model.js');
 
 let authorize = (request) => {
-  
+  console.log('1')
   return superagent.post(process.env.GITHUB_TOKEN_URI)
     .type('form')
     .send({
@@ -14,13 +14,18 @@ let authorize = (request) => {
       redirect_uri: `${process.env.API}/oauth/github`,
     })
     .then(tokenResponse => {
+      console.log('2')
+      console.log(tokenResponse.body)
       let githubToken = tokenResponse.body.access_token;
       return githubToken;
     })
     .then(githubToken => {
+      console.log('3')
+      console.log(githubToken)
       return superagent.get(process.env.GITHUB_USER_URI)
         .set('Authorization', `Bearer ${githubToken}`)
         .then( userResponse => {
+          console.log('4')
           let githubUser = userResponse.body;
           return githubUser;
         });
