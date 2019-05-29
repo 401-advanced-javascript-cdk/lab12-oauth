29f4d7d981b9691d0d91356f055a50047a8953ce
Chris Kozlowski <kozlowskicd@gmail.com>
2019-05-29 16:01:41 -0700

    refactor

--- src/auth/oauth/github.js
+++ src/auth/oauth/github.js
@@ -4,28 +4,23 @@ const superagent = require('superagent');
 const Users = require('../users-model.js');
 
 let authorize = (request) => {
-  console.log('1')
   return superagent.post(process.env.GITHUB_TOKEN_URI)
     .type('form')
     .send({
       code: request.query.code,
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
-      redirect_uri: `${process.env.API}/oauth/github`,
+      redirect_uri: `https://lab12-auth-server.herokuapp.com/oauth/github`,
     })
     .then(tokenResponse => {
-      console.log('2')
       console.log(tokenResponse.body)
       let githubToken = tokenResponse.body.access_token;
       return githubToken;
     })
     .then(githubToken => {
-      console.log('3')
-      console.log(githubToken)
       return superagent.get(process.env.GITHUB_USER_URI)
         .set('Authorization', `Bearer ${githubToken}`)
         .then( userResponse => {
-          console.log('4')
           let githubUser = userResponse.body;
           return githubUser;
         });
