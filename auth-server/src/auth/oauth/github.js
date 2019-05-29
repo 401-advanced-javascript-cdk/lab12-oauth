257b929327c09f91308e4378d5c469271d109975
Chris Kozlowski <kozlowskicd@gmail.com>
2019-05-29 13:49:07 -0700

    add github oauth

--- auth-server/src/auth/oauth/github.js
+++ auth-server/src/auth/oauth/github.js
@@ -1,1 +1,43 @@
+'use strict';
 
+const superagent = require('superagent');
+const Users = require('../users-model.js');
+
+const API = 'http://localhost:3000';
+
+let authorize = (request) => {
+  
+  console.log('(1)', request.query.code);
+  
+  return superagent.post(process.env.GITHUB_TOKEN_URI)
+    .type('form')
+    .send({
+      code: request.query.code,
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      redirect_uri: `${API}/oauth/github`,
+    })
+    .then(tokenResponse => {
+      let githubToken = tokenResponse.body.access_token;
+      return githubToken;
+    })
+    .then(githubToken => {
+      return superagent.get(process.env.GITHUB_USER_URI)
+        .set('Authorization', `Bearer ${githubToken}`)
+        .then( userResponse => {
+          let githubUser = userResponse.body;
+          return githubUser;
+        });
+    })
+    .then(githubUser => {
+      console.log('(4) Create Our Account');
+      return Users.createFromOauth(githubUser.email);
+    })
+    .then( actualUser => {
+      return actualUser.generateToken(); 
+    })
+    .catch( error => error );
+};
+
+
+module.exports = authorize;
